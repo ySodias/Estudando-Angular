cef66066a5fff876486e7cb28bfc115e8dd9bb51
Yuri Soares <yuri.soares@enforcegroup.com.br>
2021-06-08 18:18:52 -0300

    componente extrato

--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent implements OnInit {
+
+  @Output() aoTransferir = new EventEmitter<any>();
 
   valor: number;
   destino: number;
@@ -16,6 +19,18 @@ export class NovaTransferenciaComponent implements OnInit {
   }
 
   transferir(){
-   console.log('Solicitada nova transferência')
+   console.log('Solicitada nova transferência');
+   const valorEmitir = {
+    valor: this.valor,
+    destino: this.destino
+  };
+   this.aoTransferir.emit(valorEmitir);
+
+   this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
   }
 }
