bd018f7d5cacbc0718078967186b6527659209a3
ySodias <ySodia@gmail.com>
2021-06-09 07:39:08 -0300

    @input

--- bytebank/src/app/extrato/extrato.component.ts
+++ bytebank/src/app/extrato/extrato.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-extrato',
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExtratoComponent implements OnInit {
 
-  constructor() { }
+  @Input() transferencias: any[];
 
-  ngOnInit(): void {
-  }
+  constructor(){}
 
+  ngOnInit(): void{}
 }
