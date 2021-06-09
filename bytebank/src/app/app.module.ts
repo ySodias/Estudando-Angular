c2eac567763e33e91dcea4f13c9e2e91483826a8
Yuri Soares <yuri.soares@enforcegroup.com.br>
2021-06-09 18:12:48 -0300

    trabalhando com pipeline

--- bytebank/src/app/app.module.ts
+++ bytebank/src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { ExtratoComponent } from './extrato/extrato.component';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt'
+
+registerLocaleData(localePt, 'pt');
 
 @NgModule({
   declarations: [
@@ -16,7 +21,13 @@ import { ExtratoComponent } from './extrato/extrato.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt' },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
